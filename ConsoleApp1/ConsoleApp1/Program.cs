bf650a10121cf8ee967809dddcac81987efa8fce
nitins-optimus <nitin.sharma@optimusinfo.com>
2018-12-07 01:25:47 +0530

    Added Unit Test Case Exercise also.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            GradeBook book = new GradeBook();
+            book.AddGrade(23);
+            book.AddGrade(87.3f);
+            book.AddGrade(55);
+            book.AddGrade(75);
+
+            GradeStatistics stats = book.ComputeStatistics();
+
+            Console.WriteLine(stats.AverageGardes);
+            Console.WriteLine(stats.HighestGrades);
+            Console.WriteLine(stats.LowestGrades);
+            */
+
+        }
+    }
+}
---
+++ ConsoleApp1/Grades.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Grades.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Grades.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("19d3e6fe-7fc9-43c3-aaf4-c7f2da521408")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
