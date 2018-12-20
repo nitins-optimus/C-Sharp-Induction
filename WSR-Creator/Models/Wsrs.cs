9c28c8503850a125467fc3ebb036b263b540d0e1
nitins-optimus <nitin.sharma@optimusinfo.com>
2018-12-20 18:36:48 +0530

    Worked on Project Detail page

--- WSR-Creator/Models/Wsrs.cs
+++ WSR-Creator/Models/Wsrs.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace WSR_Creator.Models
 {
-    public class Wsr
+    public class Wsr: INotifyPropertyChanged
     {
         public string ProjectName { get; set; }
         public string OwnerName { get; set; }
         public string EffortSpent { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(string propertyName)
+        {
+
+        }
     }
 
    
